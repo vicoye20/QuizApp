a884d402951dde1c38a6bd7bc54cf77e0fac74a6
unknown <you@example.com>
2023-12-20 05:13:43 -0800

    my alert

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+document.getElementById("btn").addEventListener('click', function(){
+
+    let inp = document.getElementById('inp1').value
+
+    if(inp === ""){
+        alert('Pls enter your name');
+        return;
+    }
+
+    document.getElementById('div1').innerHTML = document.getElementById('div2').innerHTML
+
+    })
+
 
 
   
